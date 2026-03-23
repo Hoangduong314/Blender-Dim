7c60f500e02aed2936f049c483441d5caef5d4c0
hoangduong314 <hoangduong314@gmail.com>
2026-03-23 23:48:34 +0700

    D6 Dim V2

---
+++ constants.py
@@ -0,0 +1,37 @@
+import math
+import uuid
+import json
+import bmesh
+import mathutils.geometry
+import bpy
+import gpu
+import mathutils
+from bpy_extras import view3d_utils
+from gpu_extras.batch import batch_for_shader
+from mathutils import Matrix, Vector
+
+
+try:
+    SHADER = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
+except ValueError:
+    SHADER = gpu.shader.from_builtin('UNIFORM_COLOR')
+
+COLLECTION_NAME = "Dimensions"
+DEFAULT_STYLE_ID = "default"
+
+STYLE_COPY_FIELDS = (
+    "name",
+    "dim_unit",
+    "dim_show_suffix",
+    "dim_precision",
+    "dim_font_path",
+    "dim_text_color",
+    "dim_scale_x",
+    "dim_text_size_mm",
+    "dim_text_gap_mm",
+    "dim_ext_overshoot_mm",
+    "dim_arrow_style",
+    "dim_arrow_size_mm",
+    "dim_ext_use_fixed",
+    "dim_ext_fixed_len_mm",
+)
